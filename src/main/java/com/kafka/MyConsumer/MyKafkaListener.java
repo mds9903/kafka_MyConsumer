d69ee02633f48b4d5f1a3e7f070fa2323717e59f
mds9903 <mohammed.sujan@nextuple.com>
2023-01-15 17:22:24 +0530

    able to make external api call

--- src/main/java/com/kafka/MyConsumer/MyKafkaListener.java
+++ src/main/java/com/kafka/MyConsumer/MyKafkaListener.java
@@ -2,11 +2,20 @@ package com.kafka.MyConsumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class MyKafkaListener {
     @KafkaListener(topics = "type_2_myTopic", groupId = "type_2_myGroup")
     void listener(String data){
         System.out.println("listener received: "+data + ".");
+        try{
+            // send this data to an external api
+            RestTemplate restTemplate = new RestTemplate();
+            String result = restTemplate.getForObject("http://localhost:8090/api/myMessage/", String.class);
+            System.out.println("Response: "+result);
+        }catch (Exception e){
+            System.out.println("Exception Occurred: "+e);
+        }
     }
 }
